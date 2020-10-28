f521df8a84618bd3f50c207703d99026dab6902a
lexisbr <josebarriosrodas@gmail.com>
2020-10-28 00:39:35 -0600

    Iniciando tabla de simbolos otra vez

---
+++ Analizador_lexico/Analizador_lexico/Simbolo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analizador_lexico
+{
+    class Simbolo
+    {
+        private string tipo;
+        private string identificador;
+        private string valor;
+
+        public Simbolo(string tipo, string identificador, string valor)
+        {
+            this.tipo = tipo;
+            this.identificador = identificador;
+            this.valor = valor;
+        }
+
+        public void setValor(string valor)
+        {
+            this.valor = valor;
+        }
+
+        public string getTipo()
+        {
+            return tipo;
+        }
+    }
+}
